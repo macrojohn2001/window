333056e2803594bbcb4fdbc57bc78a06b6351038
FDS <FDS@SEARI-F>
2017-12-08 10:59:22 +0800

    添加项目文件。

---
+++ WebApp/Controllers/CacheController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CacheController : Controller
+    {
+        // GET: Cache
+        public ActionResult Index()
+        {
+            
+            this.HttpContext.Cache["table"] = "Part";
+           
+            return View();
+        }
+    }
+}
